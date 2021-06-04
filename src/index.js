f151a02bd40477acb539726dceac1418447b3100
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-06-04 16:12:58 +0000

    Stuff in the backend

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
+/* >>>>> MODULES <<<<< */
 const express = require("express");
 const fetch = require("node-fetch");
-const app = express();
+const cookieParser = require('cookie-parser')
+const auth = require('./auth.js')
+const start = (new Date()).getTime()
+
+/* >>>>> SETTINGS <<<<< */
+
 const port = 3000;
 const frontendURL = "448me.sse.codesandbox.io";
-const backEndURL = "hly3v.sse.codesandbox.io";
+const backendURL = "Backend.9gr.repl.co";
+
+
+
+const app = express();
+
+app.use(cookieParser())
 
 app.get("/", (req, res) => {
-  res.send("This is the WhenFlagClicked API!");
+  res.send({ uptime: ((new Date()).getTime() - start ) / 1000});
 });
 
 app.get("/auth/begin", (req, res) => {
   res.redirect(
     `https://fluffyscratch.hampton.pw/auth/getKeys/v2?redirect=${Buffer.from(
-      `${backEndURL}/auth/handle`
+      `${backendURL}/auth/handle`
     ).toString("base64")}`
   );
 });
@@ -27,20 +39,71 @@ app.get("/auth/handle", async (req, res) => {
   let authData = await authResponse.json();
 
   if (authData.valid) {
-    // get the proper case of the username instead of url case
+    var user = await auth.getUser(authData.username);
+    if (!user) {
+      user = await auth.createUser(authData.username)
+    }
 
-    let scratchResponse = await fetch(
-      `https://api.scratch.mit.edu/users/${authData.username}/`
-    );
-    let scratchData = await scratchResponse.json();
 
-    if (scratchData.code) res.redirect(`//${frontendURL}/login?error=0`);
+    let session = await auth.createSession(user.username)
 
-    res.redirect(`//${frontendURL}/login?token=${privateCode}`);
+    res.cookie('token', session, { path: '/' })
+
+    res.redirect(`//${frontendURL}/login?token=${session}`);
   } else {
-    res.redirect(`//${frontendURL}/login?error=0`); // failed fluffyscratch auth
-    // res.json({ error: 'failed fluffyscratch auth' }) // commented out because showing users json for a common error isnt great. instead redirecting to the frontend where they can easily log in again is best
+    res.redirect(`//${frontendURL}/login?error=1`); // failed fluffyscratch auth
   }
 });
 
-app.listen(port, () => console.log(`Listening port ${port}`));
+app.get('/auth/me', auth.authMiddleware, async (req, res) => {
+  res.json(req.user)
+})
+
+app.put('/auth/delete', auth.authMiddleware, async (req, res) => {
+  await auth.deleteSession(req.cookies.token)
+  res.status(200).json({ ok: 'logged out' })
+})
+
+app.get('/users/:user', async (req, res) => {
+  let user = await auth.getUser(req.params.user)
+  
+  user ? res.json(user) : res.status(404).json({ error: 'tutorial not found'})
+})
+
+app.put('/tutorial/new', auth.authMiddleware, async (req, res) => {
+  let matches = await db.list('tutorial-')
+
+  let id = matches.length + 1;
+
+  let Tutorial = {
+    id,
+    body: req.body.body,
+    author: req.user,
+    tags: [],
+    history: {
+      created: {
+        time: Date.now(),
+        user: req.user
+      },
+      edited: {
+        time: Date.now(),
+        user: req.user
+      }
+    }
+  }
+
+  await db.set(`tutorial-${id}`, Tutorial)
+
+  res.json(tutorial)
+})
+
+app.get('/tutorial/:id', async (req, res) => {
+  let tutorial = await db.get(`tutorial-${req.params.id}`)
+  tutorial ? res.json(tutorial) : res.status(404).json({ error: 'tutorial not found'})
+})
+
+app.use((req, res, next) => {
+  res.json({ code: "NotFound", error: `${req.path} is not a valid API endpoint`})
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
